a2ac09c6aa08e7a2d1b0dcf0ed7120da5bfe097a
Jahirul-Pse <jahirulislam7820@gmail.com>
2025-02-21 23:44:38 +0600

    Added fileOnClick eventHandler to File button...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.Graphics.Printing3D;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,20 +24,51 @@ namespace WinUIApp1
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        public MainWindow() {
             this.InitializeComponent();
-        }
-        
-        private void clickToIncrease(object sender, RoutedEventArgs e) {
-            count++;
-            
-        }
-        private void clickToDecrease(object sender, RoutedEventArgs e) {
-            count--;
-            
+            if (this.Content != null) {
+                mainRoot = this.Content.XamlRoot;
+            }
         }
 
-        private int count = 0;
+        private async void ShowInfoDialog(object sender, RoutedEventArgs e) {
+            if (mainRoot == null) {
+                mainRoot = this.Content.XamlRoot;
+            }
+
+            ContentDialog dialog = new ContentDialog {
+                Title = "Information",
+                Content = "This is a open message.",
+                CloseButtonText = "OK",
+                XamlRoot = mainRoot
+            };
+
+            await dialog.ShowAsync();
+        }
+
+        private void fileOnClick(object sender, RoutedEventArgs e) {
+            var openItem = new MenuFlyoutItem { Text = "Open" };
+            openItem.Click += (s, args) => { ShowInfoDialog(s, args); };
+
+            var saveItem = new MenuFlyoutItem { Text = "Save" };
+            saveItem.Click += (s, args) => { ShowInfoDialog(s, args); };
+
+            var exitItem = new MenuFlyoutItem { Text = "Exit" };
+            exitItem.Click += (s, args) => { Application.Current.Exit(); };
+
+            if(menuFlyout == null) {
+                menuFlyout = new MenuFlyout();
+                menuFlyout.Items.Add(openItem);
+                menuFlyout.Items.Add(saveItem);
+                menuFlyout.Items.Add(new MenuFlyoutSeparator());
+                menuFlyout.Items.Add(exitItem);
+
+            }
+            var button = sender as Button;
+            menuFlyout.ShowAt(button);
+        }
+        
+        private XamlRoot? mainRoot;
+        private MenuFlyout? menuFlyout;
     }
 }
